02994cc8a140a1f0db60e435492f353c32e7e4e1
Mikhail Knyazhev <markus621@gmail.com>
2020-02-12 03:06:08 +0300

    php-prof v0.742.0

---
+++ php-prof/demo.php
@@ -0,0 +1,23 @@
+<?php
+
+xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+
+function demo_test()
+{
+    for ($i = 0; $i <= 100; $i++) {
+        random_bytes(20);
+    }
+}
+demo_test();
+
+$xhprof_data = xhprof_disable();
+
+$XHPROF_ROOT = '/tools/xhprof/';
+include_once $XHPROF_ROOT.'/xhprof_lib/utils/xhprof_lib.php';
+include_once $XHPROF_ROOT.'/xhprof_lib/utils/xhprof_runs.php';
+
+$xhprof_runs = new \XHProfRuns_Default();
+$run_id = $xhprof_runs->save_run($xhprof_data, 'xhprof_testing');
+
+header("Location: http://localhost:9002/index.php?run={$run_id}&source=xhprof_testing");
+echo "http://localhost:9002/index.php?run={$run_id}&source=xhprof_testing";
